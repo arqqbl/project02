0932dc7ffec62bfbdc302c6c29b7a47eba78e8bb
samyrahara <marisaazzahra01@gmail.com>
2025-07-05 18:35:12 +0700

    update

--- app/Filament/Mahasiswa/Resources/RecipeResource/Pages/ListRecipes.php
+++ app/Filament/Mahasiswa/Resources/RecipeResource/Pages/ListRecipes.php
@@ -4,36 +4,31 @@ namespace App\Filament\Mahasiswa\Resources\RecipeResource\Pages;
 
 use App\Filament\Mahasiswa\Resources\RecipeResource;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Tables\Columns\TextColumn;
+use Filament\Tables;
 use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Notifications\Notification;
 
 class ListRecipes extends ListRecords
 {
     protected static string $resource = RecipeResource::class;
 
-    protected function getTableSchema(): array
+    protected function getTableColumns(): array
     {
         return [
             TextColumn::make('deskripsi')
                 ->label('Deskripsi')
                 ->wrap()
-                ->suffixAction(
-                    Action::make('tambahKeKoleksi')
+                ->suffixAction(function ($record) {
+                    return Action::make('addToCollection')
                         ->icon('heroicon-o-star') // Bintang putih
                         ->tooltip('Tambah ke Koleksi Saya')
-                        ->action(function ($record) {
-                            $user = auth('mahasiswa')->user();
+                        ->color('gray')
+                        ->action(function () use ($record) {
+                            /** @var \App\Models\Mahasiswa $mahasiswa */
+                            $mahasiswa = auth('mahasiswa')->user();
 
-                            if (!$user) {
-                                Notification::make()
-                                    ->title('Anda belum login sebagai mahasiswa')
-                                    ->danger()
-                                    ->send();
-                                return;
-                            }
-
-                            $collection = $user->collections()->firstOrCreate(
+                            $collection = $mahasiswa->collections()->firstOrCreate(
                                 ['name' => 'Koleksi Saya'],
                                 ['description' => 'Koleksi pribadi']
                             );
@@ -44,8 +39,8 @@ class ListRecipes extends ListRecords
                                 ->title('Resep berhasil ditambahkan ke Koleksi Saya')
                                 ->success()
                                 ->send();
-                        })
-                ),
+                        });
+                }),
         ];
     }
 }
